45eb6637f20b545a952ec5fbf81a2fca31de4d86
alfred <matthias.neumann@destruktiv.info>
2025-10-04 18:57:25 +0200

    feature: ctrl

--- users/halcyon_modules/splitkb/hlc_tft_display/hlc_tft_display.c
+++ users/halcyon_modules/splitkb/hlc_tft_display/hlc_tft_display.c
@@ -26,6 +26,7 @@
 static const char *caps =        "Caps";
 static const char *gui =         "Gui";
 static const char *alt =         "Alt";
+static const char *ctrl =        "Ctrl";
 
 static painter_font_handle_t Retron27;
 static painter_font_handle_t Retron27_underline;
@@ -188,24 +189,30 @@ void update_display(void) {
         Retron27_underline = qp_load_font_mem(font_Retron2000_underline_27);
     }
 
-    // Always update GUI and Alt key display
+    // Always update modifier key display
     uint8_t mods = get_mods();
+    
     bool gui_pressed = mods & (MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
     gui_pressed
-        ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 2 - 10, Retron27_underline, gui,    HSV_GUI_ON,    HSV_BLACK)
-        : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 2 - 10, Retron27, gui,    HSV_GUI_OFF,    HSV_BLACK);
+        ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 3 - 6, Retron27_underline, gui, HSV_GUI_ON, HSV_BLACK)
+        : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 3 - 6, Retron27, gui, HSV_GUI_OFF, HSV_BLACK);
 
     bool alt_pressed = mods & (MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT));
     alt_pressed
-        ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height - 5, Retron27_underline, alt, HSV_SCROLL_ON, HSV_BLACK)
-        : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height - 5, Retron27, alt, HSV_SCROLL_OFF, HSV_BLACK);
+        ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 2 - 4, Retron27_underline, alt, HSV_SCROLL_ON, HSV_BLACK)
+        : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 2 - 4, Retron27, alt, HSV_SCROLL_OFF, HSV_BLACK);
+
+    bool ctrl_pressed = mods & (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RCTL));
+    ctrl_pressed
+        ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height - 2, Retron27_underline, ctrl, HSV_SCROLL_ON, HSV_BLACK)
+        : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height - 2, Retron27, ctrl, HSV_SCROLL_OFF, HSV_BLACK);
 
     if(last_led_usb_state.raw != host_keyboard_led_state().raw || first_run_led == false) {
         led_t led_usb_state = host_keyboard_led_state();
 
         led_usb_state.caps_lock
-            ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 3 - 15, Retron27_underline, caps,   HSV_CAPS_ON,   HSV_BLACK)
-            : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 3 - 15, Retron27, caps,   HSV_CAPS_OFF,   HSV_BLACK);
+            ? qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 4 - 8, Retron27_underline, caps, HSV_CAPS_ON, HSV_BLACK)
+            : qp_drawtext_recolor(lcd_surface, 5, LCD_HEIGHT - Retron27->line_height * 4 - 8, Retron27, caps, HSV_CAPS_OFF, HSV_BLACK);
 
         last_led_usb_state = led_usb_state;
         first_run_led = true;
